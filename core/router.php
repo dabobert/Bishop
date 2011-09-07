ae06dd67f4c73a86fceed2c9f802cf3ad7a9a9fb
Rob Jenkins <bobert@gmail.com>
2011-09-07 17:03:55 -0400

    - making progress with routes

--- core/router.php
+++ core/router.php
@@ -3,4 +3,30 @@
 //bishop/core/../routes.php
 require_once dirname(__FILE__)."/../routes.php";
 
+class Router
+{
+	private		$debug;
+	protected	$routes;
+	
+	function __construct($debug=false) {
+		$this->debug = $debug;
+	}
+	
+	public function __call($data,$argument) {
+		switch ($data) {
+		case "get":
+		case "post":
+		case "destroy":
+		    $this->set_route($data,$argument[0], $argument[1]);
+		    break;
+		}
+	}
+	
+	public function set_route($method,$uri, $closure) {
+		if ($this->debug)
+			echo "adding $method method for $uri<br>\n";
+		$this->routes[$method][$uri] = $closure;
+	}
+};
+
 ?>
--- routes.php
+++ routes.php
@@ -1,17 +1,27 @@
 <?
-
+/*
 class Routes {
 	
 	public static function show() {
-
-	return array(
-		"/people"=> function(){
-			echo "<h1>hey now</h1>";
-		}
-	);}
+		return array(
+			"/people"=> function(){
+				echo "<h1>hey now</h1>";
+			}
+		);
+	}
 }
+*/
+/*
+$routes = array(
+	"/people"=> function(){
+		echo "<h1>hey now</h1>";
+	}
+);
+*/
 
-
-
+$router = new Router(true);
+$router->get('/people', function() {
+	echo "Hey";
+});
 
 ?>
