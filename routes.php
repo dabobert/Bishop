5cb4bfe0aaec29f9e9b1cfa4a207510cc23c8922
Rob Jenkins <bobert@gmail.com>
2011-09-08 11:49:58 -0400

    updated formated of routes

--- routes.php
+++ routes.php
@@ -1,27 +1,27 @@
 <?
 
-$router->get('/people:format', function() {
+$router->get('/people(.:format)', function() {
 	return "<h1>show all people</h1>";
 });
 
-$router->get('/people:id:format', function() {
+$router->get('/people:id(.:format)', function() {
 	return "<h1>show a person</h1>";
 });
 
-$router->get('/people/new:format', function() {
+$router->get('/people/new(.:format)', function() {
 	return "<h1>we gotta fix this</h1>";
 	//will just display the html form
 });
 
-$router->post('/people:format', function() {
+$router->post('/people(.:format)', function() {
 	return "<h1>create a person </h1>";
 });
 
-$router->put('/people:id:format', function() {
+$router->put('/people:id(.:format)', function() {
 	return "<h1>update a person</h1>";
 });
 
-$router->delete('/people:id:format', function() {
+$router->delete('/people:id(.:format)', function() {
 	return "<h1>delete a person</h1>";
 });
 
